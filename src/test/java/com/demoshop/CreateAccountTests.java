a9d7e6d7b6012bfb47df1a33c726a3b4960054f1
danilforx <danileremenkoschool@gmail.com>
2024-10-13 21:58:06 +0200

    4 commit

--- src/test/java/com/demoshop/CreateAccountTests.java
+++ src/test/java/com/demoshop/CreateAccountTests.java
@@ -5,15 +5,15 @@ import org.testng.annotations.Test;
 public class CreateAccountTests extends TestBase{
 
     @Test
-    public void createAccountPositiveTest() {
+    public void createAccountNegativeTest() {
         register("forex05@gmail.com", "Password1$");
     }
 
 
     @Test
     public void createAccountPositiveTest2() {
-        register("forexsws01@gmail.com", "Password1$");
+        register("forexsws02@gmail.com", "Password1$");
         logout();
-        login("forexsws01@gmail.com", "Password1$");
+        login("forexsws02@gmail.com", "Password1$");
     }
 }
